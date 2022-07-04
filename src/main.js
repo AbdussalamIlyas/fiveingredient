f855fece78068e58c7573f827c8d48fc965c37d8
AbdussalamIlyas <ilyas.abdussalam@student.htw-berlin.de>
2022-07-04 18:13:58 +0200

    Backend Base URL

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from './plugins/font-awesome'
 import authHeader from '@/services/auth-header'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://fiveingredient-backend/api'
+axios.defaults.baseURL = 'https://fiveingredient-backend.herokuapp.com/api'
 axios.defaults.headers.common = authHeader()
 
 createApp(App)
